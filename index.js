1ab3a8cd2fd0f69588b5583dbadb4b4d761668cf
moreenisaac <nkirotemoreen70@gmail.com>
2022-08-18 10:09:42 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+
+//anonymous function
+const returnFirstTwoDrivers = function(drivers) {
+    return drivers.slice(0 , 2)
+    
+}
+//anonymous function using slice to return from back
+ const returnLastTwoDrivers = function(drivers) {
+     return drivers.slice(-2);
+ }
+ //an function containg defined functions as arrays
+ const selectingDrivers = [ returnFirstTwoDrivers, returnLastTwoDrivers ];
+
+ //functions that return functions
+ const createFareMultiplier = function(intValue){
+    return function(fareValue){
+        return intValue * fareValue
+    }
+
+ }
+
+ const fareDoubler = createFareMultiplier(2);
+ const fareTripler = createFareMultiplier(3);
+
+ const selectDifferentDrivers = function(drivers , whichDrivers) {
+    return whichDrivers(drivers);
+ }
